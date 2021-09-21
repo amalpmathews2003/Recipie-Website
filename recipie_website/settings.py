14e516cfa1ec7f497ce46ecffe9813781eb1502d
Amal P Mathews <amalpmathews2003@gmail.com>
2021-09-21 18:51:57 +0530

    Update settings.py

--- recipie_website/settings.py
+++ recipie_website/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 #MEDIA_ROOT =  os.path.join(BASE_DIR, 'files')
