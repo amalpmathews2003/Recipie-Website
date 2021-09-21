5eda7ac7aa99946577830be0f77547c1cde4c20f
Amal P Mathews <amalpmathews2003@gmail.com>
2021-09-21 09:22:34 +0530

    Update models.py

--- user/models.py
+++ user/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
 	user=models.OneToOneField(User,on_delete=models.CASCADE)
 	profile_pic=models.ImageField(upload_to=f'files/{user}',
 		null=True,blank=True)
-	phone=models.CharField(max_length=20,blank=True,null=True)
+	#phone=models.CharField(max_length=20,blank=True,null=True)
 	#facebook_url=models.CharField(max_length=50,blank=True,null=True,default="None")
 	#instagram_url=models.CharField(max_length=50,blank=True,null=True)
 	#watsaap=models.CharField(max_length=50,blank=True,null=True)
